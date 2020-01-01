dff323e638055f886234c1491a0caaf46e0eb240
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-01-01 02:36:43 -0800

    changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let date = new Date();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds() 
+let minutes = date.getMinutes(); // store current minutes 
+let seconds = date.getSeconds() // store current seconds  
 
 setInterval(function () {
-   if(seconds > 60) {
+   if(seconds > 60) { 
      seconds = 1
    }
    console.log(minutes + ':' + seconds++)
