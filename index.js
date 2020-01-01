337d50c601ebf84a68510f9c97e1ee9bc94a07a6
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-01-01 02:38:59 -0800

    some changes

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ let minutes = date.getMinutes(); // store current minutes
 let seconds = date.getSeconds() // store current seconds  
 
 setInterval(function () {
+   // check seconds can not go above 60
    if(seconds > 60) { 
      seconds = 1
    }
-   console.log(minutes + ':' + seconds++)
+     
+   let formateDate = minutes + ':' + seconds++;
+   console.log(formateDate); // console output 
 }, 1000);
